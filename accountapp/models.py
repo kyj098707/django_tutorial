6af0335b2a205e8b825030452edb93f143008983
kyj098707 <kyj098707@naver.com>
2022-10-10 08:41:05 +0900

    update: connect DB

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
+    

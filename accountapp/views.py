280f948371c2cc9aa842ca8ff011e122701939f9
kyj098707 <kyj098707@naver.com>
2022-10-10 10:13:49 +0900

    update : Create Signup

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,18 @@
 from urllib.request import Request
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 from accountapp.models import HelloWorld
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User,User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def hello_world(request):
+
+
+    
 
     if request.method == "POST":
 
@@ -15,7 +22,14 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
-    else:
-        return render(request, 'accountapp/hello_world.html', context={'text': temp})
 
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'

96b5f34161f74ad1cb02f60df31987f71e392cd1
kyj098707 <kyj098707@naver.com>
2022-10-10 16:37:45 +0900

    update: add ProfileView

--- pinterest_project/settings.py
+++ pinterest_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profileapp',
     'accountapp',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
@@ -131,3 +132,6 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR,'media')

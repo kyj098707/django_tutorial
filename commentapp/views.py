4df37bac3a60eff3cab1d0a21f0bba116a39335e
kyj098707 <kyj098707@naver.com>
2022-10-20 14:07:56 +0900

    add : comment delete fucntion

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
 from commentapp.models import Comment
-from django.views.generic import CreateView
+from django.views.generic import CreateView,DeleteView
 from commentapp.forms import CommentCreationForm
 from articleapp.models import Article
-# Create your views here.
+from commentapp.decorators import comment_ownership_required
+from django.utils.decorators import method_decorator
 
 class CommentCreateView(CreateView):
     model = Comment
@@ -17,6 +18,16 @@ class CommentCreateView(CreateView):
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',kwargs={'pk':self.object.article.pk})
+
+@method_decorator(comment_ownership_required,'get')
+@method_decorator(comment_ownership_required,'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail',kwargs={'pk':self.object.article.pk})

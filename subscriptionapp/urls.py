d33cfb07ba5c3b609846f18a5b856a331fcf0052
kyj098707 <kyj098707@naver.com>
2022-10-20 23:02:39 +0900

    add : subscription list

--- subscriptionapp/urls.py
+++ subscriptionapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from subscriptionapp.views import SubscriptionView
+from subscriptionapp.views import SubscriptionView,SubscriptionListView
 
 app_name = 'subscriptionapp'
 
 urlpatterns = [
     path('subscription/',SubscriptionView.as_view(), name='subscription'),
-
+    path('list/',SubscriptionListView.as_view(), name='list'),
 ]
